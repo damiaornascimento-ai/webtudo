6a46e2af23dcc4199cadf53356ef7492ccbbae9b
User <User@ETC-INFO-13>
2026-04-20 21:33:05 -0300

    Adicionar arquivos de projeto.

---
+++ webtudo/Pages/atualizar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webtudo.Pages
+{
+    public class atualizarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ webtudo/Pages/cadastrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webtudo.Pages
+{
+    public class cadastrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
