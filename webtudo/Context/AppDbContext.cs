943bb84f5c7d3a114d2c5ea270db035082815f60
damiaornascimento-ai <damiaornascimento@gmail.com>
2026-05-25 16:26:07 -0300

    webtudo

---
+++ webtudo/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using webtudo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webtudo.Context
+{
+    // O ": DbContext" é o que faz a mágica acontecer
+    public class AppDbContext : DbContext
+    {
+        // Este construtor é necessário para o Program.cs passar as configurações
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        
+        // Aqui você vai adicionar suas tabelas depois, por exemplo:
+        // public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ webtudo/Migrations/20260525130914_CriandoProdutos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webtudo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
